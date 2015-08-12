4e76ff955585ba76b6d39c11d14cd0eaf82e722a
Agile Little2 <agile@little2.net>
2015-08-12 23:48:43 +0800

    2.0.0.0812

---
+++ config_version.php
@@ -0,0 +1,12 @@
+<?php //0046a
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
+?>
+HR+cPt7nPBG7HrsyoalnbuJVrxW7+22dHLRZHynzBXUigGk101u79oPL7fYBNxD69a5vGoq5wl04
+Q+2FKGH0xwfpVgn0Wr1JL0WeR0iJdYF71TE3/anARmMbxGpOdnuJOkLJ0nOL3/EWNeE2ZyAKvZS6
++P2sov4jvfQ8qDQ0vrcqAk0YtAOvZ1W/QXiQbPBdS+urLn5dFg5hz5pl7IXUdd5++P9qMgT3+KHW
+o1puhkkeGYg2cI+jh6xUYn0evD4QhuSaAHe4BzaO995SP5jXaDvp49l3VP6BSfwbENmxqi9u1h9G
+fKlhSu54cPdNgsYkdQH8++tG94AQQ77A42mc/Z9ZYQ1CHv8frasofZl9wzjwioL6QWX+fNISrKMq
+CNjYTg6orJ1Rlo5yCExulnl1meqjpzCXGf3k0lVXlswsFmsOQ7AtHUgUFLT4GaKkFW0o2mQvjolt
+Jz3eVzBjrz3VsdQVDLSdfVVC0slir7zUlIdRqMoGqB5L7Q5pwfgFq4UVbI6yFxs1AmG2QcS5YfSh
+/W/2nJ/ugqtawrBNsGPL4cCHGhuVj1pgEEq9bbzqz7OdpL31PxLgf+uEuwu9ceekZ8O+iAUII5SI
+2MorUV7KriNqkVHyqnC=
