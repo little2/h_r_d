060e5bdf109302224a117151dc0ef555a6245846
Agile Little2 <agile@little2.net>
2015-08-21 14:40:46 +0800

    2015-8-21
    1.修正基準制的編輯中評鑑表，會在前台出現；分級制評鑑表，即使未發佈，也會在前台出現
    2.修正評鑑調整列表，以及內容

--- ToDoList.php
+++ ToDoList.php
@@ -1,9 +1,9 @@
 <?php //0046a
 if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
 ?>
-HR+cPr5GQ/jHfM/zEEd+sBvt4NIft3jmuvuj1usiqm2sgpNCws4pPB9CBm/ufVhdlt9/2MV23l1c
-0TBvonLndXDxtkQ2DP9XBumOMZik4Ibd2xnHtI8KZ7LsvkyE/pZXL0HL+i0sWEfjtda/VQr2PK7X
-bG/E739/VLO8c/531L/355obCpvAL0b/xNVoOJVBogxIbtJSUI53mbxJhXxaCSUBghz0srns03BF
-VpuurhycmJlEEJk/YEIj6Xd5wQ0wgcYBHU1pKSp0kpTYIqKsFLnvqnstwX9gvzjVHx7DopA7vXVo
-PwuE6t1GLI+OsPIrOCSoS5/XuMZJhwnwNNS1Vn8+dRbUTGAMkOuIQ/wuSN0oFY3zzYyDLb3v0IPl
-354d71q4fK8WN1m=
+HR+cPwGWI4mFzR1ZmfdTRoRt+UDReQMX/AH17/LQmnjA1PLuV8a8B2emLhIuMsZJ7O8WWecby51V
+w+dFjAP1Jic3uW9Ht7ZOFg9vk+5kXmfbj2pw4tW1qR86tS3Q3e9YPcUzuOrdmP+ZEu/2sfm9pHaP
+s4EexgZDrGwJCJsre+xLMSwp1BL2ex52PiYD1hD7RyLQPXeL+GF0sOkrq7MTmh1K9yNNIxyManbo
+5BA0dgvM9msa2gRm9U42q3tMImHIBJM9vqmSosbj8oEJncDoJRi3GDE1LX9g77oprIuPBAu3URHR
+DxZYbjAWhOGxE2yUq8kJ6PxdY8wB0YbDXFigwpiR0PKf/nMygHlBjzZc3V+OBgVKXImRz748qluG
+8Z7FzqkrOxDw8YIv
