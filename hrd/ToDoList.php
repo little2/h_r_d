5fce2b0798c5ae810f43f2fb064f358210521c1c
Agile Little2 <agile@little2.net>
2015-09-02 21:02:43 +0800

    fixed

--- hrd/ToDoList.php
+++
@@ -1,9 +0,0 @@
-<?php //0046a
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
-?>
-HR+cPnzqOWEOm5OrSStt8Da9plq+tWMw1RV9ciCjMOGXvPUmx8AcfGGki5Yu9+EUJD6+UM2OXB4h
-aQYTTIn4IuiVTnBy0LRX+Bf1WduGsFlzDDPMrIxfCT5xRL91DjAfByg3HzETAmWI8DKDfr5KgIui
-pB2P1FVtnkr37W6JKWrBct6d2Qeo5B82Hh1UwZ1pjKMawr9VmLrwdLJiCUo8bBX34/uQXy7tLy9p
-5bYIFrw4w7wudQZH5SAgWj+r6CMr6dxeksu4RwJgSFg9OqXF/XduhHxItiWCP5AFIZ+53mG3fLkX
-9xakCsP33sduAoXbwfVzojxeDlrSOKE2jB6QGBi+qN5jMkaX3ch05uqN6YJ6CChDOXadl/EfnVQj
-MnuYe0==
--- hrd/function/edit/database_backup.php
+++
@@ -1,9 +0,0 @@
-<?php //0046a
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
-?>
-HR+cPpPE+5ovgi2JKFTjVqgiAenT+CV278EvSVymLnFXFaUtiaOCzwWHiyd5KWH45Sdp2ecTb+aJ
-jnL62xc48VR8PKKz5TmPo6+xFdZatY07bCNTIZ1jDZaGymGh+IT+uuPoibc/V05wbQv6HxyJ3+bI
-d4azv2IlE9i9tQvdtvJ/KkwfnlearrT0nVsHlbzS7pS/SrOuMfspQXiShphozg042PTA3dwNFjb3
-QW7YXqz5f29Y3ewETzo5NWFrqqRVfWapcPu7BqJxji1LT2TWXgF5wupgw2qQZDiwJmOHHRjYsTAO
-ggoHFLzNbrZt5/8oduHK2L1bGtVHGtJ7LKHK4jmwtfbXVSfUZjtWw6DPM2/Un5sltB/YFzGoLARa
-gDcN0rmVCAqB96py
