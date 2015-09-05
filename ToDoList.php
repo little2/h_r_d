bdc2ee9ce6915b58b5ba5ddfa1c5ddfdca323bf1
Agile Little2 <agile@little2.net>
2015-09-05 08:47:46 +0800

    2015-9-5 B
    1.修正分級制職能模型樹，加上行為定義及圓點

--- ToDoList.php
+++ ToDoList.php
@@ -1,9 +1,9 @@
 <?php //0046a
 if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
 ?>
-HR+cPxaLLC0vvgptqAp1uq14ysfZ+zb3p7Qp+O6iZ/rDhiazr//l7540g76gGwwoeGkxfark0UWS
-pGjw/jESUSIDyRJsKzfh7df9agkKzm4Yt1G9vK6GWsMk2BAjAPAi/h0lq1r7aoHOIfCUvr6tmwhd
-zLflbshoSk+/aYGgljp56SnIaUXM5OmMcD9SBYpANyu9MiGp4plYCgjNRVxosG4IT5OxmSZVooxV
-fj3BZ1LJIiOFaEg2ErKUFThLRBxP1hq7W6nro4tecXLfKIviycfuAREie3nXhlqCEthPRNAz3BPT
-hqeqqePJBdnUI18zQIXLsaYK2RH1YAfyLBOANQjdj5dfwy+jVZKdrfbaHng6PfFtjVFRZ39E24m8
-kpb+Vl7peJSYdge=
+HR+cPpCjHvIx2RpRLfJIjxT1vj5onFt4jEjCLO+iulfHNu9MhYb8Mvf4MZNdMgd9ITCTjGg3jWXk
+7okkAnqQQ4agu/InMLYR2GIqi4IdHYzskhrVSplPtVRpPdOJCfAeZupdQsJ1yLSAcNQG9ULfDBO5
+spRrsB95USmD4i3NwPjIWuaEx6WcGsTYaeAt2VozbOrSnftS93LKo8gJEgQEABbH476tciODucKg
+7yJhXJB4l41dagYeE0Oeki+aPfycye99Goi3SC8wFOvX3SIYDkagFSx6zFGs1Y8RI/15LFaK+2Eu
+f1y+OAHrSqHt6c/tMT+dNLtT+mTL6//lzwo3iV0T3rCjCat/ZdA9Wy8PVQTQeOXfBPR8af0i6Dyf
+IDVZrSxD4YedpQrG9YF1
