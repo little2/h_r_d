ccfb22b7db3f2f47302380ca171183ae33598649
Agile Little2 <agile@little2.net>
2015-09-21 15:21:54 +0800

    2015-9-21
    修正產生報表時,重覆的問題

--- ToDoList.php
+++ ToDoList.php
@@ -1,9 +1,9 @@
 <?php //0046a
 if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
 ?>
-HR+cPwMjS8K6ZachonJbfbiDPdwgWGNUUUs8KeUiPSMuHv+GLCpc7Bir7KX3kzxGonXCLC+eXReC
-zTd60dtdsNpoBw/2DzZn9zwzoz4W2hl267sWgfxYccM+otYykVb1uiQFtG8pabnUeYmCSESRz1Li
-vN1G4EXF6iI3Aa7SXdw0mS8nsW1KEuw2f7zyvFToE8UqB1wuLdrs4QsHKu67TQpPqlcFuq/39t2s
-DuAN6L+CNTfdFHFIeiXLKGO8UVDjOo+/pSU8MJb2sdPcSflqWV6QCKARKxaSJvnSGDINPaYyq38Y
-xx1GW8gKzuUvVnaIkR+yS8nP09ipL6mNy0SXzMVTYDm3GuEpQf9Y704n7yb9aGfqn1GAOI5eA1c/
-Mnof70==
+HR+cPuwJ1+NwPFpAbogInYII4QstjY/F/pgVnz2cKor/bimNZ7Zz7EhSsz2CGBtjgrNs++KMET0R
+SKM8+rdtzU+ma+ictYnsSxuvSTucweveJhuTbhNlvf5KTZeUzTK+26GWGVjgyH4I+q3rVFELLgUN
+n5Ybp9usDvi/1sLanr7TT23bH7jVaifo/Va2U5Np9RDV8UqMyB9ruG5CiDafWpW7+zrJmBXeIG8+
++UuxIeLEOTd9M7s/jFTolwqtvkP0czUXthW5golGffiWRVCvLRkqWZUwnRkg8oisCLPeuMX7m8HI
+PIZmp14Q0CLJJ2157IXYczuh3h/HK6z5i3ZG9kN6ZxDqy4n29vN/3pdSTmVp6yb+i860DTz3Mrrg
+4c8iQDoTWfhSx4JNxAlfolKGzl2zvQKlB1hY
