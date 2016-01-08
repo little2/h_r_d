4448aa96389f82452212dd3fabae0ea04da93463
Agile Little2 <agile@little2.net>
2016-01-08 16:59:24 +0800

    修正過慢

---
+++ test_mb.php
@@ -0,0 +1,16 @@
+<?php //0046a
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
+?>
+HR+cPxiFm9XuxZZV4RHLksHoW4iAyJVbKJCqnAUiot0OVr+ClxPCkE0KMnDD/mPusmUzvACc5zSI
+Jx+NpHXJeZZ+4qsUZk21/QqKS69pBYdYixyr5CpuCW3m2YFt2HTUXHNVJgLncRcehRFc0bKEqYHX
+Sq1vXhoXf7M64l2Wi6zN94HqdnONGoC7CWbXI0bnZ2XnnXMbrADUZorIwS19wZYKTz+6CtC5njij
+yaV96RbfAGJ8pB4diG4klQs27sO9HgK+h5CS65xNaiLQOc0lLBecn4tKunothXLqVQ3o7rdTDgpg
+Z6aE6YY712+aeUnMWvIo0JK5cuPZ+tYhi9Xtunb6P0J7MpqGfWqZBqamJbJKKK72L+UgqIO6gNZK
+96G22lojQICbkqAcNR0dvEeCtfiAAVpLOUu12yAMjgHb3uIwooIzVg4raBwz9PkJObzskQg5UI8a
+Gon+b4uB1qDS23z1KgQBvsHvViQ78TbchYReGZKQAf5cdhKEE5+JFs6YhO7a8JvRAqrRUILzPGpf
+w/TKEvRbw65hfFvDWgrcIKGYxc4k5XPYl/Flm1UEZm0Nd9Cq7O4GSIqL70Hjbo51zdYl4SBEgapY
+vzqUuqFdqPkSEFt113Os3tb/NQJZ2YYKSWVzhoxbDqN+jGYeBUJxMYYS9A/mQi6AUjXoh9ZMUfBI
+lCOZA08iDEUuqg/vng6t5GJRqQEoNJqot48DGei/5HYYDBWDBzYBKY8RLJzRhjlChVrO0SziAI1M
+enBolpb6xGeHFYmhGjgeMovP5HgaFc/8PuWY+e9wRQiGz6xX3MqP8UkLc/ztLcZ9FV6UAENLI0Zv
+6UuXu0FdUHFDLvrcVNZWsp7WizfK525dQZNQLbzLlZS2yFlsCUjc71O5Nxz4/heBLtlzVFxLP/tt
+76Ih/KQg0Bs/U+abzEsEconB6MjOC5/Ar/2Bl6KBCFcipn3N+5Q9BW8O3yekAjoGKRmt1BO+y+jW
